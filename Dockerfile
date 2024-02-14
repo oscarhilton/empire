20aad0d022a233201b250382da9356d0f899b14c
Oscar Hilton <oscarhilton@hotmail.com>
2024-02-14 00:03:33 +0000

    Foobar

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+# Use the official Node.js 16 as a parent image
+FROM node:16-alpine
+
+# Set the working directory in the container
+WORKDIR /usr/src/app
+
+# Copy package.json and package-lock.json (if available) to the container
+COPY package*.json ./
+
+# Install any dependencies
+RUN npm install
+
+# Bundle the app source inside the Docker image
+COPY . .
+
+# Make port 3000 available to the world outside this container
+EXPOSE 4000
+
+# Define the command to run the app
+CMD [ "node", "server.js" ]
---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const nunjucks = require("nunjucks");
+
+function getTimestamp() {
+  // Calculate the loop length in milliseconds (1 hour, 37 minutes, and 35 seconds)
+  const loopLength = (1 * 3600 + 37 * 60 + 35) * 1000;
+
+  const now = new Date().getTime();
+
+  // Get the current time since Brexit in milliseconds
+  const startTime =
+    now - new Date("January 31, 2020 23:00:00 GMT").getTime() / 1000;
+
+  // Calculate the current time within the loop
+  const timeInLoop = startTime % loopLength;
+
+  // Convert time in loop to hours, minutes, and seconds
+  let seconds = Math.floor((timeInLoop / 1000) % 60);
+  let minutes = Math.floor((timeInLoop / (1000 * 60)) % 60);
+  let hours = Math.floor((timeInLoop / (1000 * 60 * 60)) % 24);
+
+  // Format the time in YouTube timestamp notation
+  const formattedTime = `${hours.toString().padStart(2, "0")}h${minutes
+    .toString()
+    .padStart(2, "0")}m${seconds.toString().padStart(2, "0")}s`;
+
+  return formattedTime;
+}
+
+const app = express();
+
+nunjucks.configure("views", {
+  autoescape: true,
+  express: app,
+});
+
+app.get("/", function (req, res) {
+  res.render("index.html", { timestamp: getTimestamp() });
+});
+
+app.listen(3000);
